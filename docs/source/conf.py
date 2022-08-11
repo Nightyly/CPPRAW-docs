b9afa77df3245679d7a23ffeaebde8366271e9a7
Nightyly <asciialt59@gmail.com>
2022-08-10 19:35:09 -0500

    afgweasdr

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,10 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CPPRAW'
+author = 'Nightyly'
 
-release = '0.1'
-version = '0.1.0'
+version = '0.0.1'
 
 # -- General configuration
 
